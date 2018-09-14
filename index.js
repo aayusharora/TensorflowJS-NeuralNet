f4ff70ea96107e5614e197fe29fe12150b9da816
aayush arora <aayush113002@gmail.com>
2018-09-15 05:09:27 +0530

    Added JS files

---
+++ index.js
@@ -0,0 +1,14 @@
+const tf = require('@tensorflow/tfjs');
+const iris = require('./iris.json');
+const irisTesting = require('./testingIris.json');
+
+// convert data
+
+
+// creating model
+
+
+// compiling model
+
+
+// predicting model
