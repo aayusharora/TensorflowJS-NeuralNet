a0724a379582488b5bfac7fdc94d6eab69bad00d
aayush arora <aayush113002@gmail.com>
2018-11-23 04:55:39 +0530

    Solution Updated

--- solution/index.js
+++ solution/index.js
@@ -1,18 +1,18 @@
 const tf = require('@tensorflow/tfjs');
-const iris = require('./iris.json');
-const irisTesting = require('./testingIris.json');
+const iris = require('./training.json');
+const irisTesting = require('./testing.json');
 
 // Mapping the trainingdata
 const trainingData = tf.tensor2d(iris.map(item=> [
     item.sepal_length, item.sepal_width, item.petal_length, item.petal_width
 ]
-),[144,4])
+),[130,4])
 
 // Mapping the testing data
 const testingData = tf.tensor2d(irisTesting.map(item=> [
     item.sepal_length, item.sepal_width, item.petal_length, item.petal_width
 ]
-),[3,4])
+),[14,4])
 
 // creating model
 const outputData = tf.tensor2d(iris.map(item => [
@@ -20,40 +20,47 @@ const outputData = tf.tensor2d(iris.map(item => [
     item.species === 'virginica' ? 1 : 0,
     item.species === 'versicolor' ? 1 : 0
 
-]), [144,3])
+]), [130,3])
 
 // Creating Model
 const model = tf.sequential();
 
-// Adding Input Layer
-model.add(tf.layers.dense({
-    inputShape: [4],
-    activation: "sigmoid",
-    units: 5
-}))
+model.add(tf.layers.dense(
+    {   inputShape: 4, 
+        activation: 'sigmoid', 
+        units: 10
+    }
+));
 
-// Adding Hidden Layer
-model.add(tf.layers.dense({
-    inputShape: [4],
-    activation: "sigmoid",
-    units: 3
-}))
+model.add(tf.layers.dense(
+    {
+        inputShape: 10, 
+        units: 3, 
+        activation: 'softmax'
+    }
+));
 
-// Adding output layer
-model.add(tf.layers.dense({
-    inputShape: [3],
-    activation: "sigmoid",
-    units: 3
-}))
+model.summary();
 
 // compiling model
 model.compile({
-    loss: "meanSquaredError",
-    optimizer: tf.train.adam(.06)
+    loss: "categoricalCrossentropy",
+    optimizer: tf.train.adam()
 })
 
-// predicting model
-model.fit(trainingData, outputData, {epochs: 100})
-    .then(() => {
-        model.predict(testingData).print();
-})
+async function train_data(){
+    console.log('......Loss History.......');
+    for(let i=0;i<40;i++){
+     let res = await model.fit(trainingData, outputData, {epochs: 40});
+     console.log(`Iteration ${i}: ${res.history.loss[0]}`);
+  }
+}
+
+async function main() {
+    await train_data();
+    console.log('....Model Prediction .....')
+    model.predict(testingData).print();
+}
+
+main();
+  
